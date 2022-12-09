eb53a77d6617bcfa846b81de45ceda95c22f4626
unknown <velazql7@gmatc.matc.edu>
2022-12-08 20:40:23 -0600

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -13,6 +14,14 @@ const IndexPage = ({ data }) => (
         data.allContentfulBlogPost.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
+            <div>
+              <GatsbyImage
+                image={edge.node.heroImage.gatsbyImageData}
+                />
+            </div>
+            <div>
+              {edge.node.body.childMarkdownRemark.excerpt}
+            </div>
           </li>
         ))
 
@@ -21,11 +30,8 @@ const IndexPage = ({ data }) => (
   </Layout>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
+
+
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
@@ -38,6 +44,18 @@ export const query = graphql`
           id
           title
           slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
         }
       }
     }
